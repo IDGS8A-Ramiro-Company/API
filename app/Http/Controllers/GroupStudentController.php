dfe74d5e416df6e31ef08272a9b160c7631ea105
Angel Vargas <angel.delacruz@janusautomation.com>
2024-08-05 02:38:51 -0600

    modifique la funcion de asignar un grupo a un alumno

--- app/Http/Controllers/GroupStudentController.php
+++ app/Http/Controllers/GroupStudentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\GroupStudentRequest;
 use App\Models\Group_Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GroupStudentController extends Controller
 {
@@ -13,10 +14,36 @@ class GroupStudentController extends Controller
 
     }
 
-    public function create(GroupStudentRequest $request)
+    public function create(Request $request)
     {
-        $groupStudent = Group_Student::create($request->validated());
-        $groupStudent->save();
-        return response()->json(["success" => true, "data" => $groupStudent,"status" => 200]);
+        // Validar los datos de entrada
+        $request->validate([
+            'group_id' => 'required|exists:groups,id',
+            'student_id' => 'required|exists:users,id',
+        ]);
+
+        $idGroup = $request->input('group_id');
+        $idStudent = $request->input('student_id');
+
+        $existingRecord = DB::table('group_students')->where('student_id', $idStudent)->first();
+
+        if ($existingRecord) {
+            DB::table('group_students')
+                ->where('student_id', $idStudent)
+                ->update(['group_id' => $idGroup]);
+        } else {
+            // Si no existe, insertar un nuevo registro
+            DB::table('group_students')->insert([
+                'group_id' => $idGroup,
+                'student_id' => $idStudent,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+        return response()->json(['message' => 'Operation successful'], 200);
+        //$groupStudent = Group_Student::create($request->validated());
+        //$groupStudent->save();
+       // return response()->json(["success" => true, "data" => $groupStudent,"status" => 200]);
     }
 }
