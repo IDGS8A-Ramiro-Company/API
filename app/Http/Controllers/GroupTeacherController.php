debb65e877f93860038c9ec7c5675937638a8978
Angel Vargas <angel.delacruz@janusautomation.com>
2024-08-05 00:27:24 -0600

    asignar grupo a un alumno fix

--- app/Http/Controllers/GroupTeacherController.php
+++ app/Http/Controllers/GroupTeacherController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\GroupTeacherRequest;
 use Illuminate\Http\Request;
 
 class GroupTeacherController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function create(GroupTeacherRequest $request)
+    {
+        $groupTeaccher =
+    }
 }
