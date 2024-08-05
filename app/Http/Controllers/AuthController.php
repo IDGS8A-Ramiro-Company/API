a0799d28ffda916c556d05b7194534f1d1c65637
Angel Vargas <angel.delacruz@janusautomation.com>
2024-08-04 22:44:38 -0600

    el login ahora devuelve el rol

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
             return response()->json([
                 'success' => true,
                 'remember_token'=>$user->getRememberToken(),
-                'rol'=> $userauth
+                'rol'=> $userauth->id_rol
             ]);
         }
         return response()->json([
